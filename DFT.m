54abfd23c0fe06eeae9096f6666e7524df875338
dmw1998 <dmw1998@users.noreply.github.com>
2020-09-04 19:10:29 +0800

    Update DFT.m

--- DFT.m
+++ DFT.m
@@ -1,11 +1,9 @@
-function transform_matrix = DFT(x)
-
+function m = DFT(x)
 N = length(x);
-
-n = [0:N-1]
-
+n = [0:N-1];
 k = n';
 
-transform_matrix = exp(-2*i*pi*n.*k./N)
+transform_matrix = exp(-2*1i*pi*n.*k./N);
 
+m = transform_matrix.*x;
 end
